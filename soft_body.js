74f2742d7b09350974c5aa7177c20239c3a0e82a
acetim <Lilevii1978@gmail.com>
2026-02-14 16:23:29 +0200

    final project

--- soft_body.js
+++ soft_body.js
@@ -1,5 +1,21 @@
 //להוסיף אינטרקציה עם העכבר ולעשות שזה משנה את הבאונדס להמלבן הקטן לעולם כולו ובנסוף בכל פעם שאני עושה את זה לשנות את המשתנה isbounded שלפיו הלוק נקבע וגם לשנות אותו לטרו באפדייט באונדס
 
+
+/*
+LOCK UNLOCK FUNCTIONALITY
+
+checklock is called from piece and cheks each frame if the velocity is low enough and if the piece is bounded - if it is it locks the piece
+
+bounds are added and removed whena piece is selected and a turn has ended
+
+unlock occurs when a piece is selected
+
+
+
+*/
+
+
+
 class Softbody {
   constructor(points, drawcurve,lock,color) {
     this.Vec2D = toxi.geom.Vec2D;
@@ -9,7 +25,7 @@ class Softbody {
     this.VerletSpring2D = toxi.physics2d.VerletSpring2D;
     this.GravityBehavior = toxi.physics2d.behaviors.GravityBehavior;
     this.physics = new this.VerletPhysics2D();
-     // a point used to measure the avg velocity of the entire piece for the sake of locking it 
+      
     this.drawcurve = true;
     this.points = points;
     this.color = color
@@ -23,9 +39,9 @@ class Softbody {
     this.isbounded = true;
     
     this.maxpointx = this.particles[0];
-this.minpointx = this.particles[0];
-this.maxpointy = this.particles[0];
-this.minpointy = this.particles[0];
+    this.minpointx = this.particles[0];
+    this.maxpointy = this.particles[0];
+    this.minpointy = this.particles[0];
 
 for (let i = 0; i < this.particles.length; i++) {
   const p = this.particles[i];//find the highest ,lowest ,right and left points in order to make boundary checking easier
@@ -92,30 +108,30 @@ for (let i = 0; i < this.particles.length; i++) {
     }
     else {
       
- if (this.color == "black"){
-   fill("#537D5D");
- }
- if (this.color == "white"){
-   fill("#F4FFC3");
- }    
-  stroke("#18230F");
-strokeWeight(2);
-  beginShape();
+       if (this.color == "black"){
+         fill("#537D5D");
+       }
+       if (this.color == "white"){
+         fill("#F4FFC3");
+       }    
+       stroke("#18230F");
+       strokeWeight(2);
+       beginShape();
+
+ 
+       curveVertex(this.particles[this.particles.length - 2].x, this.particles[this.particles.length - 2].y);
+       curveVertex(this.particles[this.particles.length - 1].x, this.particles[this.particles.length - 1].y);
   
  
-  curveVertex(this.particles[this.particles.length - 2].x, this.particles[this.particles.length - 2].y);
-  curveVertex(this.particles[this.particles.length - 1].x, this.particles[this.particles.length - 1].y);
-  
- 
-  for (let p of this.particles) {
-    curveVertex(p.x, p.y);
-  }
+       for (let p of this.particles) {
+          curveVertex(p.x, p.y);
+       }
 
   
-  curveVertex(this.particles[0].x, this.particles[0].y);
-  curveVertex(this.particles[1].x, this.particles[1].y);
-  
-  endShape(CLOSE);
+       curveVertex(this.particles[0].x, this.particles[0].y);
+       curveVertex(this.particles[1].x, this.particles[1].y);
+
+       endShape(CLOSE);
   }
 }
   
@@ -140,14 +156,14 @@ strokeWeight(2);
 }
 
 
-  updateBounds() {//allowed=if the move is allowed
-    let p = this.translatecords();
-    if (p != false &&(!mouseIsPressed)&&(!this.isbounded)){
-      let bounds = new this.Rect(p[0] * 100, p[1] * 100, 100, 100);
-      this.physics.setWorldBounds(bounds);
-      this.isbounded=true
-    }
-  }
+  // updateBounds() {
+  //   let p = this.translatecords();
+  //   if (p != false &&(!mouseIsPressed)&&(!this.isbounded)){
+  //     let bounds = new this.Rect(p[0] * 100, p[1] * 100, 100, 100);
+  //     this.physics.setWorldBounds(bounds);
+  //     this.isbounded=true
+  //   }
+  // }
   
   setbounds(x,y){
     let r = new this.Rect(x*100,y*100,100,100);
@@ -162,7 +178,7 @@ strokeWeight(2);
   this.isbounded = false
  
   }
-  checklock(){//checks is the velocity is low enough to lock
+  checklock(){//checks is the  avg velocity is low enough to lock
     
     
     if(postframecount >60){
@@ -172,11 +188,11 @@ strokeWeight(2);
   let prevX1 = this.minpointy.prev.x;
   let prevY1 = this.minpointy.prev.y;
 
-  // Calculate velocity components
+  
   let velocityX1 = currentX1 - prevX1;
   let velocityY1 = currentY1 - prevY1;
 
-  // Calculate speed (magnitude of velocity)
+  
   let speed3 = dist(0, 0, velocityX1, velocityY1);
       
       
@@ -189,11 +205,11 @@ strokeWeight(2);
   let prevX2 = this.maxpointx.prev.x;
   let prevY2 = this.maxpointx.prev.y;
 
-  // Calculate velocity components
+  
   let velocityX2 = currentX2 - prevX2;
   let velocityY2 = currentY2 - prevY2;
 
-  // Calculate speed (magnitude of velocity)
+  
   let speed1 = dist(0, 0, velocityX2, velocityY2);
         
         
@@ -202,11 +218,11 @@ strokeWeight(2);
   let prevX3 = this.minpointx.prev.x;
   let prevY3 = this.minpointx.prev.y;
 
-  // Calculate velocity components
+  
   let velocityX3 = currentX3 - prevX3;
   let velocityY3 = currentY3 - prevY3;
 
-  // Calculate speed (magnitude of velocity)
+  
   let speed2 = dist(0, 0, velocityX3, velocityY3);
           
           
@@ -220,11 +236,11 @@ strokeWeight(2);
   let prevX4 = this.maxpointy.prev.x;
   let prevY4 = this.maxpointy.prev.y;
 
-  // Calculate velocity components
+  
   let velocityX4 = currentX4 - prevX4;
   let velocityY4 = currentY4 - prevY4;
 
-  // Calculate speed (magnitude of velocity)
+  
   let speed4 = dist(0, 0, velocityX4, velocityY4);
   
     if(this.isbounded&&Math.abs(speed1)<0.1&&Math.abs(speed2)<0.1&&Math.abs(speed3)<0.1&&Math.abs(speed4)<0.1){
--- start.js
+++ start.js
@@ -1,49 +1,43 @@
-var boids=[]
+var boids = []
 ///////////////////////////
 let angle = 0;
 
 ////////////////////////
-function start(arr,n,startTimer){
-  
-  if(mouseIsPressed&& mouseX>width-70&&mouseX<width-20&&mouseY>100&&mouseY<150&&f==true){
-        return false;
-    
-      }
-  
+function start() {
 
-  
-  
+  if (mouseIsPressed && mouseX > width - 70 && mouseX < width - 20 && mouseY > 100 && mouseY < 150 && f == true) {
+    return false;
 
-
-
+  }
   background('#077A7D');
-  
-  
-  for(let b of boids){
-     b.applyForce(b.avoidAll())
-     b.applyForce(b.align(boids).mult(3))
-     b.applyForce(b.cohesion(boids).mult(2))
-     b.applyForce(b.seperate(boids).mult(2))
-    if(mouseIsPressed){
-     b.applyForce(b.seekmouse().mult(10))
+  for (let b of boids) {
+    b.applyForce(b.avoidAll())
+    b.applyForce(b.align(boids).mult(3))
+    b.applyForce(b.cohesion(boids).mult(2))
+    b.applyForce(b.seperate(boids).mult(2))
+    if (mouseIsPressed) {
+      b.applyForce(b.seekmouse().mult(10))
 
     }
-     b.update()
+    b.update()
   }
-  
-  for(let b of boids){
-  b.show()
+
+  for (let b of boids) {
+    b.show()
   }
- fill(30, 30, 3); 
-  rect(width-80, 110, 50, 50, 5);
-  
+  fill(30, 30, 3);
+  rect(width - 80, 110, 50, 50, 5);
+
   fill(245, 196, 94);
-  rect(width-70,100,50,50,5);
+  rect(width - 70, 100, 50, 50, 5);
   textSize(40)
   textFont("Courier New")
-  text("softbody chess",400,550)
+  text("softbody chess", 400, 550)
+  textSize(20)
+  text("by koren uri and snir", 400, 580)
+  
   return true;
 
-  
+
 }
 ///////////////////////////
